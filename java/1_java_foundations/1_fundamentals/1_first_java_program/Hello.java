c5811902a3d390f45f316bb5884fe4dfd5fa5980
Danny Cho <danheecho@gmail.com>
2022-06-03 14:22:01 -0700

    Completed Java / Java Foundations / Fundamentals / First Java Program.

---
+++ java/1_java_foundations/1_fundamentals/1_first_java_program/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("My name is Danny Cho.");
+        System.out.println("I am 30 years old.");
+        System.out.println("My hometwon is Seoul, Korea.");
+    }
+}
