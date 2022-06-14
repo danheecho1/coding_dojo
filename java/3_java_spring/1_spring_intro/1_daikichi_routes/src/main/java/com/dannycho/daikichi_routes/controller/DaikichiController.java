b5d2272102d427d8ea99b75f482dea19dc18a161
Danny Cho <danheecho@gmail.com>
2022-06-13 23:59:01 -0700

    Completed Java / Java Spring / Spring Intro / Daikichi Routes assignment

---
+++ java/3_java_spring/1_spring_intro/1_daikichi_routes/src/main/java/com/dannycho/daikichi_routes/controller/DaikichiController.java
@@ -0,0 +1,25 @@
+package com.dannycho.daikichi_routes.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/daikichi")
+public class DaikichiController {
+	
+	@RequestMapping("")
+	public String homepage() {
+		return "Welcome!";
+	}
+	
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	
+	@RequestMapping("/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+}
