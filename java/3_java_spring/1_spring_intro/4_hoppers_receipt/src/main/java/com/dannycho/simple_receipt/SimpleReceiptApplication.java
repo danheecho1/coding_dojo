f8d7dc3e295f6b7a9f270ecdc91589fb61b8356d
Danny Cho <danheecho@gmail.com>
2022-06-14 00:27:46 -0700

    Completed Java / Java Spring / Spring Intro / Hopper's Receipt

---
+++ java/3_java_spring/1_spring_intro/4_hoppers_receipt/src/main/java/com/dannycho/simple_receipt/SimpleReceiptApplication.java
@@ -0,0 +1,13 @@
+package com.dannycho.simple_receipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleReceiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleReceiptApplication.class, args);
+	}
+
+}
---
+++ java/3_java_spring/1_spring_intro/4_hoppers_receipt/src/test/java/com/dannycho/simple_receipt/SimpleReceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dannycho.simple_receipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleReceiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
